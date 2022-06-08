871967039c9258da3589cd41f407fe6f996b6876
wayne <xutenglongxutenglong@outlook.com>
2022-06-08 10:57:12 +0800

    完成了商品列表页面的开发 git commit -m 完成了商品列表页面的开发“

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,19 +1,77 @@
 <template>
-  <view>
-    Goods_List
-  </view>
+	<view>
+		<view class="goods-list">
+			<view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
+				<my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        
-      };
-    }
-  }
+	export default {
+		data() {
+			return {
+				//请求参数对象
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize: 10
+				},
+				goodsList: [],
+				total: 0,
+				//节流阀
+				isloading:false
+				};
+		},
+		onLoad(options) {
+			console.log(options);
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			this.getGoodsList()
+		},
+		methods: {
+			//获取商品列表数据的方法
+			async getGoodsList(cb) {
+				//打开节流阀
+				this.isloading = true
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				//关闭节流阀
+				this.isloading = false
+				cb && cb()
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.goodsList = [...this.goodsList,...res.message.goods]
+				this.total = res.message.total
+			},
+			onReachBottom(){
+				if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕')
+				if(this.isloading) return
+				//让页码值自增加一
+				this.queryObj.pagenum++
+				this.getGoodsList()
+			},
+			onPullDownRefresh(){
+				//重置关键数据
+				this.queryObj.pagenum = 1
+				this.total = 0
+				this.isloading = false
+				this.goodsList = []
+				this.getGoodsList(()=>uni.stopPullDownRefresh())
+				
+			},
+			//点击跳转到商品详情页面
+			gotoDetail(goods){
+				uni.navigateTo({
+					url:'/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+				})
+			}
+		}
+	}
 </script>
 
 <style lang="scss">
-
+	
 </style>
